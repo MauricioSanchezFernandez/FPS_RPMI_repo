fd384efbb96976a5af88c94b886e4e43661b6b41
MauricioSanchezFernandez <mausanfer16@gmail.com>
2026-03-12 13:02:16 +0100

    ai

--- RPMI_3EVA/Assets/_FPS_RPMI/scripts/Enemy/EnemyAIBase.cs
+++ RPMI_3EVA/Assets/_FPS_RPMI/scripts/Enemy/EnemyAIBase.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.AI; //libreria  decomponente NavMesh
 public class EnemyAIBase : MonoBehaviour
@@ -17,7 +18,7 @@ public class EnemyAIBase : MonoBehaviour
 
     [Header("Attacking Stats")]
     [SerializeField] float timeBetweenAttacks = 1f; //cooldown entre ataques
-    [SerializeField] GameObject projectile; //ref a la bala fisica que dispara al enemigo
+    [SerializeField] GameObject proyectile; //ref a la bala fisica que dispara al enemigo
     [SerializeField] Transform shootpoint; //posicion desde la que se disparo la bala
     [SerializeField] float shootSpeedY; //fuerza de disparo hacia arriba (catapulta)
     [SerializeField] float shootSpeedZ = 10f; //fuerza de disparo hacia delante (siempre se necesita)
@@ -25,7 +26,7 @@ public class EnemyAIBase : MonoBehaviour
 
 
     [Header("States & Detection")]
-    [SerializeField] float signtRange = 8f; //radio del detector de persecucion
+    [SerializeField] float signhtRange = 8f; //radio del detector de persecucion
     [SerializeField] float attackRange = 2f; //radio del detector del ataque
     [SerializeField] bool targetInSightRange; //determina si e sverdadero que podemos perseguir al target
     [SerializeField] bool targetInAttacktRange; //determina si e sverdadero que podemos atacar al target
@@ -42,6 +43,14 @@ public class EnemyAIBase : MonoBehaviour
     #endregion
 
 
+    private void Awake()
+    {
+        target = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        lastPosition = transform.position;
+        lastCheckTime = Time.time;
+    }
+
     void Start()
     {
         
@@ -50,6 +59,97 @@ public class EnemyAIBase : MonoBehaviour
 
     void Update()
     {
-        
+        EnemyStateUpdater();
     }
+
+    void EnemyStateUpdater()
+    {
+        //Metodo que se encarga de gestionar el cambio de estados del enemigo
+
+
+        //1 - cambio de estado de los bools
+        Collider[] hits = Physics.OverlapSphere(transform.position, signhtRange, targetLayer);
+        targetInSightRange = hits.Length > 0;
+        //segundo si estan en vision detectamos si ademas estan en ataque
+        if (targetInSightRange)
+        { 
+            float distance = Vector3.Distance(transform.position, target.position);
+            targetInAttacktRange = distance <= attackRange;
+        }
+        else 
+        {
+            targetInAttacktRange = false;
+        }
+
+
+
+        //2 - cambio de estados segun booleanos
+        if (!targetInSightRange && !targetInAttacktRange)
+        {
+            Patroling();
+        }
+
+        else if (targetInSightRange && !targetInAttacktRange)
+        {
+
+            ChaseTarget();
+        }
+
+        else if (targetInSightRange && targetInAttacktRange)
+        {
+            AttackTarget();
+        }
+    }
+
+    void Patroling()
+    {
+        Debug.Log("Enemigo en estado patrulla");
+    }
+
+    void ChaseTarget()
+    {
+        //Accion que le dice al agente que persiga al target 
+        agent.SetDestination(target.position);
+    }
+
+    void AttackTarget()
+    {
+        //accion que contiene la logica de ataque
+        //1- hacer que el agente se quede quieto (perseguirse a asi mismo)
+        agent.SetDestination(transform.position);
+
+        //2 aplicar una rotacion suavizada para que mire al target antes de atacar
+        Vector3 direction = (target.position - transform.position).normalized;
+        if (direction != Vector3.zero)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, agent.angularSpeed * Time.deltaTime);
+        }
+
+        //3 Se ataca solo si no se esta atacando
+        if (!alreadyAttacked)
+        {
+            Rigidbody rb = Instantiate(proyectile, shootpoint.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * shootSpeedZ, ForceMode.Impulse);
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    void ResetAttack()
+    { 
+        alreadyAttacked = false;
+    
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (Application.isPlaying) return; //Si estamos jugando en build no se ejecuta el resto del codigo
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, signhtRange);
+
+    }
+
 }
